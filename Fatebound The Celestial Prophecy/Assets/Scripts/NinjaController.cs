11ee44d7cfdfea7f581686a5052ffb98247acc37
Negraru Celestin <celestin-andrei.negraru@s.unibuc.ro>
2024-11-06 13:23:10 +0200

    OK

--- Fatebound The Celestial Prophecy/Assets/Scripts/NinjaController.cs
+++ Fatebound The Celestial Prophecy/Assets/Scripts/NinjaController.cs
@@ -22,26 +22,51 @@ public class NinjaController : MonoBehaviour
         //setam parametrii din animator(combinatii de 1 si -1)
         animator.SetFloat("moveX", moveX);
         animator.SetFloat("moveY", moveY);
-
+        if(moveX<0)
+        {
+            transform.localScale = new Vector3((float)-0.32, (float)0.32, 0);
+        }
+        else if(moveX>0)
+        {
+            transform.localScale = new Vector3((float)0.32, (float)0.32, 0);
+        }
         // este evident ca pentru a trece din starea de idle la running va trebui sa se miste pe cel putin una din cele doua axe, adica produsul moveX*moveY sa fie diferit de 0
         bool isRunning = moveX != 0 || moveY != 0;
+        bool isDash = false;
+        bool isCrouch = false;
         animator.SetBool("isRunning", isRunning);
 
         // daca se apasa Q
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            animator.SetBool("isCrouch", true);
-        }
-        //daca se elibereaza tasta Q
-        else if (Input.GetKeyUp(KeyCode.Q))
-        {
-            animator.SetBool("isCrouch", false);
-        }
+            if (animator.GetBool("isCrouch") == false)
 
+            {
+                isCrouch = true;
+                animator.SetBool("isCrouch", true);
+
+            }
+            else
+            {
+                animator.SetBool("isCrouch", false);
+                isCrouch = false;
+            }
+        }
+            if (animator.GetBool("isCrouch")==true&&animator.GetBool("isRunning")==true)
+            {
+            animator.SetBool("isDash", true);
+            }
+            else if(animator.GetBool("isCrouch") == true && animator.GetBool("isRunning") == false)
+        {
+            animator.SetBool("isDash", false);
+        }
         // click stanga pentru atac
         if (Input.GetMouseButtonDown(0))
         {
             animator.SetTrigger("attack");
         }
     }
+
+
+    
 }
